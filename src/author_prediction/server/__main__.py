128894c5361908632e174271d499579df714fc9b
Joseph Geis <josephegeis@gmail.com>
2026-08-31 18:25:15 -0600

    refactor app.py

--- src/author_prediction/server/__main__.py
+++ src/author_prediction/server/__main__.py
@@ -1,2 +1,2 @@
-from .app import main
-main()
+import uvicorn
+uvicorn.run("author_prediction.server:app", host="0.0.0.0", port=8000, reload=True)
--- src/author_prediction/server/app.py
+++ src/author_prediction/server/app.py
@@ -6,13 +6,3 @@ app = FastAPI(title="Author Prediction API")
 @app.get("/")
 def read_root() -> dict[str, str]:
     return {"message": "Hello, World!"}
-
-
-def main() -> None:
-    import uvicorn
-
-    uvicorn.run("author_prediction.server:app", host="0.0.0.0", port=8000, reload=True)
-
-
-if __name__ == "__main__":
-    main()
